ca5f997b7c00bf28b4b64709d2a17e94c7aa0d93
MiJou <hsiaomirou@gmail.com>
2026-05-23 11:11:37 +0200

    load only male passengers

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 def load_data():
-    """Load the titanic dataset and return it as a DataFrame."""
+    """Load titanic dataset and return only male passengers."""
     df = pd.read_csv('data/titanic.csv')
+    df = df[df['Sex'] == 'male']
     return df
